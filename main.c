edea9f45eaaabb571fb6e280ef959a4a0571113b
Corbin <94539385+Nibroc6@users.noreply.github.com>
2026-09-18 11:01:34 -0400

    output testing

--- main.c
+++ main.c
@@ -51,6 +51,12 @@ void GPIOInit(){
     GPIO_initDigitalInput(GPIOB, GPIO_PIN21); // init bumper 4
     GPIO_initDigitalInput(GPIOB, GPIO_PIN23); // init bumper 5
     GPIO_initDigitalInput(GPIOB, GPIO_PIN25); // init bumper 6
+    GPIO_setInternalResistor(GPIOA, GPIO_PIN7, GPIO_PULL_UP); // init bumper 1 pur
+    GPIO_setInternalResistor(GPIOA, GPIO_PIN14, GPIO_PULL_UP); // init bumper 2 pur
+    GPIO_setInternalResistor(GPIOB, GPIO_PIN5, GPIO_PULL_UP); // init bumper 3 pur
+    GPIO_setInternalResistor(GPIOB, GPIO_PIN21, GPIO_PULL_UP); // init bumper 4 pur
+    GPIO_setInternalResistor(GPIOB, GPIO_PIN23, GPIO_PULL_UP); // init bumper 5 pur
+    GPIO_setInternalResistor(GPIOB, GPIO_PIN25, GPIO_PULL_UP); // init bumper 6 pur
     
     GPIO_initDigitalInput(GPIOB, GPIO_PIN22); // init rgb 1
     GPIO_initDigitalInput(GPIOB, GPIO_PIN26); // init rgb 2
@@ -69,11 +75,37 @@ void testIO(){
     // Example code for testing outputs
     while(1){
         uint8_t cmd = getchar();
-        if(cmd == 'r'){
-            // Turn RGB LED Red
-        }else if(cmd == 'g'){
-            // Turn RGB LED Green
-        }// ...
+        switch (cmd) {
+            case 'R':
+                GPIO_setPins(GPIOB, GPIO_PIN26);
+                break;
+            case 'r':
+                GPIO_clearPins(GPIOB, GPIO_PIN26);
+                break;
+            case 'G':
+                GPIO_setPins(GPIOB, GPIO_PIN27);
+                break;
+            case 'g':
+                GPIO_clearPins(GPIOB, GPIO_PIN27);
+                break;
+            case 'B':
+                GPIO_setPins(GPIOB, GPIO_PIN22);
+                break;
+            case 'b':
+                GPIO_clearPins(GPIOB, GPIO_PIN22);
+                break;
+            case 'q':
+                GPIO_clearPins(GPIOB, GPIO_PIN12);
+                GPIO_setPins(GPIOB, GPIO_PIN13);
+                break;
+            case 'e':
+                GPIO_clearPins(GPIOB, GPIO_PIN13);
+                GPIO_setPins(GPIOB, GPIO_PIN12);
+                break;
+            case 'w':
+                GPIO_setPins(GPIOB, GPIO_PIN12 | GPIO_PIN13);
+                break;
+        }
     }
 }
 
